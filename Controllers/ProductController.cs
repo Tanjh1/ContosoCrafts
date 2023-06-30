e9d050b6f122dd56060888ba1197ce24fcb3e0e6
Tanjh1 <137674126+Tanjh1@users.noreply.github.com>
2023-06-30 09:23:32 +0800

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using ContosoCrafts.Models;
+using ContosoCrafts.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoCrafts.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : Controller
+    {
+        public ProductController(JsonFileProductsService productService) =>
+            ProductService = productService;
+
+        public JsonFileProductsService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get() => ProductService.GetProducts();
+
+        [HttpPatch]
+        public ActionResult Patch([FromBody] RatingRequest request)
+        {
+            if (request?.ProductId == null)
+                return BadRequest();
+
+            ProductService.AddRating(request.ProductId, request.Rating);
+
+            return Ok();
+        }
+
+        public class RatingRequest
+        {
+            public string? ProductId { get; set; }
+            public int Rating { get; set; }
+        }
+    }
+}
